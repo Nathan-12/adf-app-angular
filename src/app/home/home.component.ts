d4d54f3658524984b7e9121d7996405f7f6aa362
Nathan Lima <nathanlima.b@gmail.com>
2021-01-05 16:00:02 -0300

    Finalizado o método edit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CaixaModel } from '../model/caixa.model';
 import { ServiceService } from '../service/service.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AlfrescoApi, GroupsApi } from '@alfresco/js-api';
+import { SiteModel } from '@alfresco/adf-core';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +13,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class HomeComponent implements OnInit {
 
   public caixaForm: FormGroup;
+  public caixaFormEdit: FormGroup;
   caixa = {} as CaixaModel;
   caixas: CaixaModel[];
 
@@ -22,7 +25,12 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.getCaixas();
+    //this.getGroups();
     this.caixaForm = this.fb.group({
+      name: ['', [Validators.required]],
+    });
+
+    this.caixaFormEdit = this.fb.group({
       name: ['', [Validators.required]],
     });
   }
@@ -31,8 +39,35 @@ export class HomeComponent implements OnInit {
     this.caixaService.getCaixas().subscribe((caixas: CaixaModel[]) => {
       this.caixas = caixas;
       console.log(this.caixas);
+      this.getGroups();
     });
   }
+
+  getGroups(){
+    const alfrescoApi = new AlfrescoApi({
+      hostEcm: 'http://localhost:4200'
+    });
+    const groupsApi = new GroupsApi(alfrescoApi);
+    groupsApi.listGroups().then((data) => {
+      console.log('Chamada a API feita com sucesso: ' + data.list.entries[5].entry.displayName);
+    }, function(error){
+      console.log(error);
+    })
+  }
+
+  // addCaixaWithNameGroup(){
+  //   const alfrescoApi = new AlfrescoApi({
+  //     hostEcm: 'http://localhost:4200'
+  //   });
+  //   const groupsApi = new GroupsApi(alfrescoApi);
+
+  //   groupsApi.listGroups().then((data) => {
+  //     //console.log('Chamada a API feita com sucesso: ' + data.list.entries[5].entry.displayName);
+  //     this.rest.saveCaixa(data.list.entries[5].entry.displayName)
+  //   }, function(error){
+  //     console.log(error);
+  //   })
+  // }
 
   createCaixa() {
     if(this.caixaForm.value.name != null){
@@ -44,6 +79,7 @@ export class HomeComponent implements OnInit {
     }
     this.caixaForm.reset();
     this.getCaixas();
+    window.location.reload();
   }
 
   deleteCaixa(caixa: CaixaModel) {
@@ -54,17 +90,21 @@ export class HomeComponent implements OnInit {
 
   openModal(caixa: CaixaModel){
     this.caixa = { ...caixa};
+     console.log(this.caixa);
+  }
+
+  openModalDelete(caixa: CaixaModel){
+    this.caixa = { ...caixa};
   }
 
   editCaixa(caixa: CaixaModel) {
-    
-    this.caixa = { ...caixa };
-    console.log("this.caixa: " + this.caixa);
     this.rest.updateCaixas(caixa).subscribe(() => {
-      
       this.caixa = caixa;
       console.log(this.caixa.id)
     })
+    this.getCaixas();
+    this.caixaFormEdit.reset();
+    window.location.reload();
   }
 
   
